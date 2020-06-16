daf8186adfb86c330787de322abd512bd3531296
Jesse P. Johnson <jpj6652@gmail.com>
2020-06-15 22:05:33 -0400

    Troubleshooting bump

--- compendium/__init__.py
+++ compendium/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-"""Simple configuration management tool"""
+'''Simple configuration management tool'''
 
-__title__ = "compendium"
-__version__ = "0.1.0"
-__license__ = "Apache-2.0"
+__title__ = 'compendium'
+__version__ = '0.1.0'
+__license__ = 'Apache-2.0'
 
-__all__ = ["Settings"]
+__all__ = ['ConfigPaths', 'Settings']
