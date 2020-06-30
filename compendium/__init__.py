8e7fa6ebf68c1f841705cdca69040644cd455c4b
kuwv <kuwv@workcentre.mgmt.hiynx.io>
2020-06-30 15:42:54 -0400

    Bump version: 0.1.1-dev43 → 0.1.1-dev44

--- compendium/__init__.py
+++ compendium/__init__.py
@@ -2,7 +2,7 @@
 '''Simple configuration management tool'''
 
 __title__ = 'compendium'
-__version__ = '0.1.1-dev43'
+__version__ = '0.1.1-dev44'
 __license__ = 'Apache-2.0'
 
 __all__ = ['ConfigPaths', 'ConfigManager', 'Settings']
