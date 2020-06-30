0d9d95c4ddcb39d9c44d90cb8ff0e0ce3d2ce8a5
kuwv <kuwv@workcentre.mgmt.hiynx.io>
2020-06-30 09:07:56 -0400

    Bump version: 0.1.1-dev36 → 0.1.1-dev37

--- compendium/__init__.py
+++ compendium/__init__.py
@@ -2,7 +2,7 @@
 '''Simple configuration management tool'''
 
 __title__ = 'compendium'
-__version__ = '0.1.1-dev36'
+__version__ = '0.1.1-dev37'
 __license__ = 'Apache-2.0'
 
 __all__ = ['ConfigPaths', 'ConfigManager', 'Settings']
--- tests/test_version.py
+++ tests/test_version.py
@@ -2,4 +2,4 @@ from compendium import __version__
 
 
 def test_version():
-    assert __version__ == "0.1.1-dev36"
+    assert __version__ == "0.1.1-dev37"
