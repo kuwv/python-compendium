a8088602f5d9b6dc60b25a85c8b7018659360894
Jesse P. Johnson <jpj6652@gmail.com>
2020-07-01 21:05:37 -0400

    Bump version: 0.1.1-dev52 → 0.1.1-dev53

--- compendium/__init__.py
+++ compendium/__init__.py
@@ -2,7 +2,7 @@
 '''Simple configuration management tool'''
 
 __title__ = 'compendium'
-__version__ = '0.1.1-dev52'
+__version__ = '0.1.1-dev53'
 __license__ = 'Apache-2.0'
 
 __all__ = ['ConfigPaths', 'ConfigManager', 'Settings']
