43a18235f31b034fdb11cabb75ca7d066d5f543b
Jesse P. Johnson <jpj6652@gmail.com>
2020-07-18 16:49:13 -0400

    added engironment tests and anymod

--- tests/test_environment_settings.py
+++ tests/test_environment_settings.py
@@ -6,7 +6,7 @@ from compendium.settings import Settings
 
 def test_environment(fs):
     '''Test environment variables.'''
-    os.environ['TESTS_KEY'] = 'test' 
+    os.environ['TESTS_KEY'] = 'test'
     env = Settings(application='tests')
     env.load_environment()
     assert env.get('/env/**/key') == 'test'
