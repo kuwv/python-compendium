d742a7701eccf1d0cf989dab2e84cf44ec33a40d
Jesse P. Johnson <jpj6652@gmail.com>
2020-07-18 18:24:36 -0400

    Bump version: 0.1.1-dev55 → 0.1.1-dev56

--- compendium/__init__.py
+++ compendium/__init__.py
@@ -3,7 +3,7 @@
 
 __author__ = 'Jesse P. Johnson'
 __title__ = 'compendium'
-__version__ = '0.1.1-dev55'
+__version__ = '0.1.1-dev56'
 __license__ = 'Apache-2.0'
 
 __all__ = ['ConfigPaths', 'ConfigManager', 'Settings']
