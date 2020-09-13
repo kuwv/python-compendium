e471769227a9c11775142376899e15b1e3ae0580
Jesse P. Johnson <jpj6652@gmail.com>
2020-09-12 23:05:46 -0400

    Refactor for version file and CI

--- compendium/__init__.py
+++ compendium/__init__.py
@@ -3,13 +3,8 @@
 # license: Apache 2.0, see LICENSE for more details.
 '''Simple configuration management tool.'''
 
-__author__ = 'Jesse P. Johnson'
-__title__ = 'compendium'
-__version__ = '0.1.1-dev63'
-__license__ = 'Apache-2.0'
-
-__all__ = ['ConfigPaths', 'ConfigManager', 'Settings']
-
 import logging
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__all__ = ['ConfigPaths', 'ConfigManager', 'Settings']
---
+++ compendium/__version__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# copyright: (c) 2020 by Jesse Johnson.
+# license: Apache 2.0, see LICENSE for more details.
+'''Inspection based parser based on argparse.'''
+
+__author__ = 'Jesse P. Johnson'
+__author_email__ = 'jpj6652@gmail.com'
+__title__ = 'compendium'
+__description__ = 'Inspection based parser built on argparse.'
+__version__ = '0.1.1-dev63'
+__license__ = 'Apache-2.0'
+__copyright__ = 'Copyright 2020 Jesse Johnson.'
