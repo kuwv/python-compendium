55573a6f5a2d0cb18ea498cafa1cd47c2918c135
Jesse P. Johnson <jpj6652@gmail.com>
2020-09-18 22:29:54 -0400

    Added multiprocessing

--- compendium/config_manager.py
+++ compendium/config_manager.py
@@ -3,25 +3,38 @@
 # license: Apache 2.0, see LICENSE for more details.
 '''Provide configuration manager for multiple settings.'''
 
-# import logging
-# import os
-# from typing import List, Optional, Union
+import logging
+import os
+from typing import List, Optional, Union
 
-# from multiprocessing import Process
+import multiprocessing
 
-from .settings import Settings
+from .cache import SettingsCache
+
+
+def worker():
+    '''worker function'''
+    print('Worker')
+    return
 
 
 class ConfigManager:
     '''Manage multiple disperate settings groups.'''
-    pass
-    # def __init__(self, *args, **kwargs):
-    #     '''Inialize configuration manager.'''
-    #     self.configs: dict = {}
-    #     for config in kwargs.get('configs'):
-    #         self.add_config(config)
 
-    # def add_config(self, config: dict):
-    #     '''Add configuration cache to manage.'''
-    #     self.configs.append({config['application']: Settings(**config)})
-    #     self.configs[config['application']].load()
+    def __init__(self, *args, **kwargs):
+        '''Inialize configuration manager.'''
+        self.caches: list = []
+
+        self.application = kwargs.get('application')
+        configs = kwargs.get('configs')
+        for config in configs:
+            p = multiprocessing.Process(target=worker)
+            self.caches.append(p)
+            # self.caches.append({
+            #     config['filename'], self.add_config(config)
+            # })
+            p.start()
+
+    def add_config(self, config: dict):
+        '''Add configuration cache to manage.'''
+        return SettingsCache(self.application, **config).load()
---
+++ tests/test_config_manager.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# :copyright: (c) 2020 by Jesse Johnson.
+# :license: Apache 2.0, see LICENSE for more details.
+'''Test settings management.'''
+
+import os
+
+import pytest  # type: ignore
+
+from compendium.config_manager import ConfigManager
+
+config_path = os.path.dirname(os.path.realpath(__file__))
+settings_path = config_path + '/settings.toml'
+
+
+# @pytest.mark.parametrize('fs', [[['pkgutil']]], indirect=True)
+def test_toml_delete():  # fs):
+    '''Test content deletion.'''
+    # fs.add_real_file(settings_path, False)
+    cfg = ConfigManager(
+        application='tests',
+        configs=[
+            {'filename': 'settings.toml', 'path': settings_path}
+        ]
+    )
+    # cfg.load()
+    # assert cfg.search('/owner/name') == ['Tom Preston-Werner']
+    # cfg.delete('/owner/name')
+    # assert cfg.search('/owner/name') == []
