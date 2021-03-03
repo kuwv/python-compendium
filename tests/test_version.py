ee649fe05359bd2f197fe112c4b83f9e602cb497
Jesse P. Johnson <jpj6652@gmail.com>
2021-03-02 19:28:54 -0500

    Bump version: 0.1.1-dev69 → 0.1.1-dev70

--- tests/test_version.py
+++ tests/test_version.py
@@ -8,4 +8,4 @@ from compendium.__version__ import __version__
 
 def test_version():
     '''Test project version is managed.'''
-    assert __version__ == "0.1.1-dev69"
+    assert __version__ == "0.1.1-dev70"
